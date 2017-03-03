db509ee748b75c5f5fc0a12082986680b486c910
Ricardo Santos <rickardosf@live.com>
2017-03-03 08:27:17 -0300

    Começando o Desafio 2 Pet Shop

---
+++ app/src/main/java/br/com/fiap/desafio2/RM77078/petshop/PetShop.java
@@ -0,0 +1,13 @@
+package br.com.fiap.desafio2.RM77078.petshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PetShop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_shop);
+    }
+}
